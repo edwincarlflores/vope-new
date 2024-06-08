b91307bef8a2930f392ca46359e4672f77275bfe
Edwin Carl Flores <edwincarlflores@gmail.com>
2024-06-09 03:51:08 +1200

    Add more handler kitchensink

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
 import { api } from "./api/api";
 
@@ -6,6 +6,24 @@ const app = new Elysia()
   .use(staticPlugin({ prefix: "/", assets: "./client/dist" }))
   .use(api)
   .get("/healthcheck", () => "Hello Elysia")
+  .get(
+    "/test",
+    ({ error, request }) => {
+      if (!request.url) return error(404, "Not found");
+      return { test: "Successful", code: 200 };
+    },
+    {
+      response: {
+        200: t.Object({ test: t.String(), code: t.Number() }),
+        404: t.String(),
+      },
+    },
+  )
+  .onError(({ code, redirect }) => {
+    if (code === "NOT_FOUND") {
+      return redirect("/");
+    }
+  })
   .listen(3000);
 
 console.log(
